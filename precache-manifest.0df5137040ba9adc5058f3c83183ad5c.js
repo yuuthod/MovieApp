9ca1c86131f6fff7b200c5cde601bf1a34a25cca
yuuthod <yuuthod@gmail.com>
2019-04-20 14:14:47 +0900

    Updates

---
+++ precache-manifest.0df5137040ba9adc5058f3c83183ad5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cd4007f537c85769148",
+    "url": "/MovieApp/static/css/main.48a26e65.chunk.css"
+  },
+  {
+    "revision": "9cd4007f537c85769148",
+    "url": "/MovieApp/static/js/main.fa299483.chunk.js"
+  },
+  {
+    "revision": "dedbc3b85a4301ff89be",
+    "url": "/MovieApp/static/js/runtime~main.9917d67f.js"
+  },
+  {
+    "revision": "5a27f02ebed0550894e1",
+    "url": "/MovieApp/static/js/2.ad2bdde8.chunk.js"
+  },
+  {
+    "revision": "80ff47115a618bb43a48669fbc279cd6",
+    "url": "/MovieApp/index.html"
+  }
+];
